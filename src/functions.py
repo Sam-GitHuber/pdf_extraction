3819e2cfa8f4dfc6b1b217297f368139aa7881ff
Sam-GitHuber <samejefford@gmail.com>
2025-03-21 19:32:45 +0000

    got first pdf text extraction working for simple pdf document

--- src/functions.py
+++ src/functions.py
@@ -1,4 +1,24 @@
+"""
+Module for getting text from PDF files
+"""
 import pathlib
+import pymupdf
 
 def get_data_path():
+    """
+    Get the path to the data directory
+    """
     return pathlib.Path(__file__).parent.parent.joinpath('data')
+
+def read_pdf(file_name: str):
+    """
+    Read text from a PDF file
+    """
+    data_path = get_data_path()
+    input_pdf_path = data_path.joinpath('input', file_name)
+
+    output_text = ''
+    with pymupdf.open(input_pdf_path) as document:
+        for page in document:
+            output_text += page.get_text()
+    return output_text
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,7 +1,12 @@
-from src.functions import get_data_path
+from src.functions import get_data_path, read_pdf
 
 def test_get_data_path():
     data_path = get_data_path()
     assert data_path.is_dir()
     assert data_path.name == 'data'
     assert data_path.parts[-2:] == ('pdf_extraction', 'data')
+
+def test_read_pdf_simple():
+    result = read_pdf("minimal-document.pdf")
+    assert 'Lorem ipsum dolor sit amet' in result
+

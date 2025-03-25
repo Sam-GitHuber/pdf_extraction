f637dd34f76e61440a291692c3eb7319d02937fa
Sam-GitHuber <samejefford@gmail.com>
2025-03-25 08:12:40 +0000

    attemptong to mock the to_csv function

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,14 @@
 from src.main import main
+from unittest.mock import patch
+import pandas as pd
 
-def test_main():
-    "this test simply checks if the main function runs without errors"
+# mock the pd.to_csv function
+@patch('pandas.DataFrame.to_csv')
+def test_main(mock_to_csv):
+    "this test checks if the main function writes the DataFrame to a CSV file"
     main()
+    mock_to_csv.assert_called_once()
+
+# def test_main():
+#     "this test simply checks if the main function runs without errors"
+#     main()
